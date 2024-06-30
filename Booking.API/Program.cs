8fd05e4bf28442dcdc1d44f0ec589fdf0165f980
Łukasz Bednarczyk <lk.bednarczyk@gmail.com>
2024-07-01 00:58:19 +0200

    Update Program.cs

--- Booking.API/Program.cs
+++ Booking.API/Program.cs
@@ -8,13 +8,18 @@ using Booking.API.Infrastructure.Repositories;
 using FluentValidation;
 using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 
 var builder = WebApplication.CreateBuilder(args);
  
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Booking API", Version = "v1" });
+
+});
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseInMemoryDatabase("BookingDb"));
@@ -43,8 +48,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Booking API v1");
+    c.RoutePrefix = ""; // Wartość pusta, aby Swagger był dostępny pod głównym adresem
+ });
+
 app.UseHttpsRedirection();
 app.UseAuthorization();
+app.UseRouting();
 
 app.MapControllers();
 
